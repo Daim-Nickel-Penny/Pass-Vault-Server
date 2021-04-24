63a30439d9c31e5b4bca544f29169dc3d7487bd1
1804449@kiit.ac.in <1804449@kiit.ac.in>
2021-04-24 20:52:24 +0530

    Encryption IV error sorted. Decyper Done!

--- EncryptWare.js
+++ EncryptWare.js
@@ -9,9 +9,25 @@ const encrypt = (password) => {
     cypher.final(),
   ]);
 
-  return encryptedPassword.toString("hex");
+  return {
+    iv: iv.toString("hex"),
+    password: encryptedPassword.toString("hex"),
+  };
 };
 
-const decrypt = (encryption) => {};
+const decrypt = (encryption) => {
+  const decypher = crypto.createDecipheriv(
+    "aes-256-ctr",
+    Buffer.from(secure),
+    Buffer.from(encryption.iv, "hex")
+  );
+
+  const decryptedPassword = Buffer.concat([
+    decypher.update(Buffer.from(encryption.password, "hex")),
+    decypher.final(),
+  ]);
+
+  return decryptedPassword.toString("utf-8");
+};
 
 module.exports = { encrypt, decrypt };
